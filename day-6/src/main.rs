1b533c88ad699deacd807bf5a7e15fa2da9190e2
joeggg <47352958+joeggg@users.noreply.github.com>
2024-12-09 23:52:32 +0000

    day 6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -4,9 +4,11 @@ use aoc_tools::run_solution;
 
 fn main() {
     let map = read_input("input.txt");
-    run_solution(|| get_distinct_positions(&map), 1);
+    run_solution(|| get_num_visited_positions(&map), 1);
+    run_solution(|| get_number_loops(&map), 2);
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     Up,
     Down,
@@ -24,7 +26,21 @@ impl Direction {
         }
     }
 
-    fn apply_move(&self, coords: &(usize, usize)) -> (i64, i64) {
+    fn reverse(&self) -> Self {
+        match self {
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+            Self::Left => Self::Right,
+            Self::Right => Self::Left,
+        }
+    }
+
+    fn apply_move(
+        &self,
+        coords: &(usize, usize),
+        max_i: &i64,
+        max_j: &i64,
+    ) -> Option<(usize, usize)> {
         let mut new_coords = (coords.0 as i64, coords.1 as i64);
         match self {
             Direction::Up => {
@@ -40,14 +56,30 @@ impl Direction {
                 new_coords.1 += 1;
             }
         }
-        new_coords
+        if new_coords.0 < 0 || new_coords.0 >= *max_i || new_coords.1 < 0 || new_coords.1 >= *max_j
+        {
+            None
+        } else {
+            Some((new_coords.0 as usize, new_coords.1 as usize))
+        }
     }
 }
 
-fn get_distinct_positions(map: &[Vec<char>]) -> i64 {
+fn get_num_visited_positions(map: &[Vec<char>]) -> usize {
+    let (obstacle_coords, guard_coords) = get_obstacle_and_guard_coords(map);
+    let path_taken = get_path_taken(map, &obstacle_coords, guard_coords);
+
+    let mut unique_positions = HashSet::new();
+    path_taken.iter().for_each(|(coords, _)| {
+        unique_positions.insert(coords);
+    });
+
+    unique_positions.len()
+}
+
+fn get_obstacle_and_guard_coords(map: &[Vec<char>]) -> (HashSet<(usize, usize)>, (usize, usize)) {
     let mut obstacle_coords = HashSet::new();
     let mut guard_coords = (0, 0);
-    let mut guard_dir = Direction::Up;
 
     // Get obstacle and guard coords
     for (i, row) in map.iter().enumerate() {
@@ -61,29 +93,94 @@ fn get_distinct_positions(map: &[Vec<char>]) -> i64 {
             }
         }
     }
+    (obstacle_coords, guard_coords)
+}
 
+fn get_path_taken(
+    map: &[Vec<char>],
+    obstacle_coords: &HashSet<(usize, usize)>,
+    mut guard_coords: (usize, usize),
+) -> Vec<((usize, usize), Direction)> {
     let max_i = map.len() as i64;
     let max_j = map[0].len() as i64;
-    let mut visited_locations = HashSet::from([guard_coords]);
+    let mut guard_dir = Direction::Up;
+    let mut visited_locations = vec![(guard_coords, guard_dir)];
 
-    loop {
-        let coords = guard_dir.apply_move(&guard_coords);
-
-        if coords.0 < 0 || coords.0 >= max_i || coords.1 < 0 || coords.1 >= max_j {
-            break;
-        }
-
-        if obstacle_coords.contains(&(coords.0 as usize, coords.1 as usize)) {
+    while let Some(coords) = guard_dir.apply_move(&guard_coords, &max_i, &max_j) {
+        if obstacle_coords.contains(&coords) {
             guard_dir = guard_dir.turn_right();
         } else {
-            guard_coords = (coords.0 as usize, coords.1 as usize);
-            visited_locations.insert(guard_coords);
+            guard_coords = coords;
+            visited_locations.push((guard_coords, guard_dir));
         }
     }
-
     //print_map(map, &visited_locations);
+    visited_locations
+}
 
-    visited_locations.len() as i64
+fn get_number_loops(map: &[Vec<char>]) -> usize {
+    let mut loop_obstacles = HashSet::new();
+    let max_i = map.len() as i64;
+    let max_j = map[0].len() as i64;
+    let (obstacle_coords, guard_coords) = get_obstacle_and_guard_coords(map);
+    let positions = get_path_taken(map, &obstacle_coords, guard_coords);
+    let mut path_so_far = HashSet::new();
+
+    for (coords, dir) in positions {
+        if coords == guard_coords || loop_obstacles.contains(&coords) {
+            path_so_far.insert((coords, dir));
+            continue;
+        }
+        // Imagine an obstacle is at coords, step back one and turn
+        let mut curr_coords = dir.reverse().apply_move(&coords, &max_i, &max_j).unwrap();
+        let mut curr_dir = dir.turn_right();
+        let mut curr_path = path_so_far.clone();
+
+        // Project path to check for loop
+        while let Some(new_coords) = curr_dir.apply_move(&curr_coords, &max_i, &max_j) {
+            if obstacle_coords.contains(&new_coords) || new_coords == coords {
+                curr_dir = curr_dir.turn_right();
+            } else if curr_path.contains(&(new_coords, curr_dir)) {
+                // Rejoining path already travelled
+                // TODO: fix weird check needed here to reduce the overestimate
+                if test_loop(map, &obstacle_coords, guard_coords, coords) {
+                    loop_obstacles.insert(coords);
+                }
+                break;
+            } else {
+                curr_coords = new_coords;
+                curr_path.insert((curr_coords, curr_dir));
+            };
+        }
+        path_so_far.insert((coords, dir));
+    }
+    loop_obstacles.len()
+}
+
+fn test_loop(
+    map: &[Vec<char>],
+    obstacle_coords: &HashSet<(usize, usize)>,
+    mut guard_coords: (usize, usize),
+    new_obstacle_coords: (usize, usize),
+) -> bool {
+    let max_i = map.len() as i64;
+    let max_j = map[0].len() as i64;
+    let mut curr_dir = Direction::Up;
+    let mut curr_path = HashSet::new();
+
+    // Project path to check for loop
+    while let Some(new_coords) = curr_dir.apply_move(&guard_coords, &max_i, &max_j) {
+        if obstacle_coords.contains(&new_coords) || new_coords == new_obstacle_coords {
+            curr_dir = curr_dir.turn_right();
+        } else if curr_path.contains(&(new_coords, curr_dir)) {
+            // Rejoining path already travelled
+            return true;
+        } else {
+            guard_coords = new_coords;
+            curr_path.insert((guard_coords, curr_dir));
+        };
+    }
+    false
 }
 
 fn print_map(map: &[Vec<char>], visited_positions: &HashSet<(usize, usize)>) {
