46d12e03981ce30fccf43eea30631baa689fabee
joeggg <47352958+joeggg@users.noreply.github.com>
2024-12-10 23:10:37 +0000

    day 8 part 2

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -4,10 +4,11 @@ use aoc_tools::run_solution;
 
 fn main() {
     let map = read_input("input.txt");
-    run_solution(|| get_freq_to_antinodes(&map).len(), 1);
+    run_solution(|| get_num_antinodes(&map, false), 1);
+    run_solution(|| get_num_antinodes(&map, true), 2);
 }
 
-fn get_freq_to_antinodes(map: &[Vec<char>]) -> HashSet<(usize, usize)> {
+fn get_num_antinodes(map: &[Vec<char>], use_resonant_harmonics: bool) -> usize {
     let mut freq_to_locs: HashMap<char, Vec<(i64, i64)>> = HashMap::new();
 
     for (i, row) in map.iter().enumerate() {
@@ -36,16 +37,43 @@ fn get_freq_to_antinodes(map: &[Vec<char>]) -> HashSet<(usize, usize)> {
                 let delta_y = y2 - y1;
                 let delta_x = x2 - x1;
 
-                for (y, x) in [(y2 + delta_y, x2 + delta_x), (y1 - delta_y, x1 - delta_x)] {
-                    if x >= 0 && x < max_x && y >= 0 && y < max_y {
-                        antinodes.insert((y as usize, x as usize));
+                if use_resonant_harmonics {
+                    // Iterate gradient steps until hitting the edge
+                    // First step back from p1 then forward from p2
+                    let (mut x, mut y) = (*x1, *y1);
+                    loop {
+                        if x >= 0 && x < max_x && y >= 0 && y < max_y {
+                            antinodes.insert((y as usize, x as usize));
+                            x -= delta_x;
+                            y -= delta_y;
+                        } else {
+                            break;
+                        }
+                    }
+                    (x, y) = (*x2, *y2);
+                    loop {
+                        if x >= 0 && x < max_x && y >= 0 && y < max_y {
+                            antinodes.insert((y as usize, x as usize));
+                            x += delta_x;
+                            y += delta_y;
+                        } else {
+                            break;
+                        }
+                    }
+                } else {
+                    // Move 1 step away from each antenna using the gradient
+                    for (y, x) in [(y2 + delta_y, x2 + delta_x), (y1 - delta_y, x1 - delta_x)] {
+                        if x >= 0 && x < max_x && y >= 0 && y < max_y {
+                            antinodes.insert((y as usize, x as usize));
+                        }
                     }
                 }
             }
         }
     }
+    print_map(map, &antinodes);
 
-    antinodes
+    antinodes.len()
 }
 
 fn print_map(map: &[Vec<char>], antinodes: &HashSet<(usize, usize)>) {
