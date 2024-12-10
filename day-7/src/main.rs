ba6f24d4541fe43afec4caf144741be0fb7ea389
joeggg <47352958+joeggg@users.noreply.github.com>
2024-12-10 01:29:36 +0000

    day 7 part 2

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -4,23 +4,33 @@ use aoc_tools::run_solution;
 
 fn main() {
     let data = read_input("input.txt");
-    run_solution(|| get_total_calibration(&data), 1);
+    run_solution(|| get_total_calibration(&data, false), 1);
+    run_solution(|| get_total_calibration(&data, true), 2);
 }
 
-// too high answer atm
-fn get_total_calibration(data: &[(i64, Vec<i64>)]) -> i64 {
+fn get_total_calibration(data: &[(i64, Vec<i64>)], use_third_operator: bool) -> i64 {
     let mut result = 0;
 
     for (expected_total, nums) in data {
         let current_total = nums[0];
-        if check_equation(&nums[1..], current_total, *expected_total) {
+        if check_equation(
+            &nums[1..],
+            current_total,
+            *expected_total,
+            use_third_operator,
+        ) {
             result += expected_total;
         }
     }
     result
 }
 
-fn check_equation(nums: &[i64], current_total: i64, expected_total: i64) -> bool {
+fn check_equation(
+    nums: &[i64],
+    current_total: i64,
+    expected_total: i64,
+    use_third_operator: bool,
+) -> bool {
     if nums.is_empty() {
         return false;
     }
@@ -30,7 +40,7 @@ fn check_equation(nums: &[i64], current_total: i64, expected_total: i64) -> bool
         return true;
     }
 
-    if check_equation(&nums[1..], new_total, expected_total) {
+    if check_equation(&nums[1..], new_total, expected_total, use_third_operator) {
         return true;
     }
 
@@ -39,8 +49,21 @@ fn check_equation(nums: &[i64], current_total: i64, expected_total: i64) -> bool
         return true;
     }
 
-    if check_equation(&nums[1..], new_total, expected_total) {
+    if check_equation(&nums[1..], new_total, expected_total, use_third_operator) {
         return true;
+    }
+
+    if use_third_operator {
+        let new_total = (current_total.to_string() + &nums[0].to_string())
+            .parse()
+            .unwrap();
+        if nums.len() == 1 && new_total == expected_total {
+            return true;
+        }
+
+        if check_equation(&nums[1..], new_total, expected_total, use_third_operator) {
+            return true;
+        }
     }
 
     false
