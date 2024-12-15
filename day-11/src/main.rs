95912c09d1c107b74f1ff38f74e3d7c1fd02d30f
joeggg <47352958+joeggg@users.noreply.github.com>
2024-12-15 23:34:26 +0000

    day 11 part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,37 +1,61 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 use aoc_tools::run_solution;
 
 fn main() {
     let stones = read_input("input.txt");
     run_solution(|| get_num_stones(&stones, 25), 1);
+    run_solution(|| get_num_stones(&stones, 75), 1);
 }
 
 fn get_num_stones(stones: &[String], blinks: u32) -> usize {
-    let mut current_stones = stones.to_vec();
-    //println!("{:?}", current_stones);
+    let mut cache = HashMap::new();
+    stones
+        .iter()
+        .map(|stone| get_num_stones_after_blinks(stone, blinks, &mut cache))
+        .sum()
+}
 
-    for _ in 0..blinks {
-        let mut next_stones = Vec::new();
-        for stone in current_stones {
-            if stone == "0" {
-                // change to 1
-                next_stones.push("1".to_string());
-            } else if stone.len() % 2 == 0 {
-                // split in 2
-                let (first, second) = stone.split_at(stone.len() / 2);
-                next_stones.push((first.parse::<u64>().unwrap()).to_string());
-                next_stones.push((second.parse::<u64>().unwrap()).to_string());
-            } else {
-                // multiply by 2024
-                let new_val = stone.parse::<u64>().unwrap() * 2024;
-                next_stones.push(new_val.to_string())
-            }
-        }
-        current_stones = next_stones;
-        //println!("{:?}", current_stones);
+fn get_num_stones_after_blinks(
+    start_stone: &String,
+    blinks: u32,
+    cache: &mut HashMap<(String, u32), usize>,
+) -> usize {
+    let key = (start_stone.clone(), blinks);
+    if let Some(cached) = cache.get(&key) {
+        return *cached;
     }
-    current_stones.len()
+
+    if blinks == 0 {
+        return 0;
+    }
+
+    let mut new_stones = Vec::new();
+    if start_stone == "0" {
+        // change to 1
+        new_stones.push("1".to_string());
+    } else if start_stone.len() % 2 == 0 {
+        // split in 2
+        let (first, second) = start_stone.split_at(start_stone.len() / 2);
+        new_stones.push((first.parse::<u64>().unwrap()).to_string());
+        new_stones.push((second.parse::<u64>().unwrap()).to_string());
+    } else {
+        // multiply by 2024
+        let new_val = start_stone.parse::<u64>().unwrap() * 2024;
+        new_stones.push(new_val.to_string())
+    }
+
+    let sum = if blinks == 1 {
+        new_stones.len()
+    } else {
+        new_stones
+            .into_iter()
+            .map(|s| get_num_stones_after_blinks(&s, blinks - 1, cache))
+            .sum()
+    };
+
+    cache.insert(key, sum);
+    sum
 }
 
 fn read_input(filename: &str) -> Vec<String> {
